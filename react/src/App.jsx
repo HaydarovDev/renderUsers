06363a86ca49fd65fe0a54402da3fe65753e8c51
asadbek haydarov <asadbekcoder1@gmail.com>
2025-04-27 16:51:22 +0500

    update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,61 +3,82 @@ import { useState } from "react";
 
 const users = [
   {
-    fullName: "Matthew Zboncak IV",
+    firstName: "Chelsea Homenick",
+    lastName: "O'Connell",
+    categories: "family",
+    phone: "1-855-691-5087 x295",
     id: "1",
-    category: "family",
   },
   {
-    fullName: "Myron Zulauf",
-    category: "relatives",
+    firstName: "Donna Schiller",
+    lastName: "Collier",
+    categories: "family",
+    phone: "229.243.6225 x1765",
     id: "2",
   },
   {
-    fullName: "June Bauch IV",
-    category: "friends",
+    firstName: "Cornelius Spinka Sr.",
+    lastName: "Langosh",
+    categories: "family",
+    phone: "(661) 426-1902",
     id: "3",
   },
   {
-    fullName: "Christy Von DVM",
-    category: "friends",
+    firstName: "Victoria Kutch",
+    lastName: "Kozey",
+    categories: "family",
+    phone: "1-619-205-9857",
     id: "4",
   },
   {
-    fullName: "Dr. Tom Tillman MD",
-    category: "relatives",
+    firstName: "Gregg Christiansen",
+    lastName: "Kuphal",
+    categories: "relatives",
+    phone: "1-745-720-5111 x86583",
     id: "5",
+  },
+  {
+    firstName: "Dr. Marvin Lesch V",
+    lastName: "Ledner",
+    categories: "relatives",
+    phone: "577-447-7290 x6042",
+    id: "6",
+  },
+  {
+    firstName: "Gladys Runte",
+    lastName: "Rau",
+    categories: "relatives",
+    phone: "825.309.9547 x6566",
+    id: "7",
   },
 ];
 
 function App() {
   const [search, setSearch] = useState("");
-  const [addUser, setUser] = useState([]);
 
   const filteredUsers = users.filter((user) => {
     if (search == "") {
-      return users;
+      return true;
     } else {
-      return user.fullName.toLowerCase().includes(search.toLowerCase());
+      return user.lastName.toLowerCase().includes(search.toLowerCase());
     }
   });
-
-  // const adduser =
 
   return (
     <div>
       <div className="form">
+        <input type="text" placeholder="Enter firstname..." />
         <input
           type="text"
-          placeholder="Enter firstname..."
-          onChange={(e) => setUser(e.target.value)}
+          placeholder="Enter lastname..."
+          onChange={(e) => setLastName(e.target.value)}
         />
-        <input type="text" placeholder="Enter firstname..." />
         <select name="" id="">
           <option value="family">Family</option>
-          <option value="friends">Friends</option>
+          <option value="family">Friends</option>
           <option value="relatives">Relatives</option>
         </select>
-        <input type="text" placeholder="Enter firstname..." />
+        <input type="number" placeholder="Enter phone number" />
       </div>
 
       <h1>Users</h1>
@@ -71,10 +92,11 @@ function App() {
         {filteredUsers.map((user) => {
           return (
             <li key={user.id}>
-              {user.id}) {user.fullName}
+              {user.id}) {user.firstName}
+              {user.lastName}
               <span>
-                {user.category.charAt(0).toLocaleUpperCase() +
-                  user.category.slice(1)}
+                {user.categories.charAt(0).toLocaleUpperCase() +
+                  user.categories.slice(1)}
               </span>
             </li>
           );
